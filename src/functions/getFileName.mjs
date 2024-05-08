e1e444131b421f1c23fc5a2fba9c8e19cb59e0d3
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-08 17:44:19 -0400

    Add function `getFileName`

---
+++ src/functions/getFileName.mjs
@@ -0,0 +1,32 @@
+import { default as pathlib } from "path";
+import getAbsolutePath from "./getAbsolutePath.mjs";
+import isDirectory from "./isDirectory.mjs";
+
+
+/**
+ * Get the file extension of a path.
+ *
+ * @public
+ * @since v0.1.1
+ * @version 1.0.0-alpha
+ *
+ * @param {string} path
+ * The path to get the file extension of.
+ * @returns
+ * The file extension of the path.
+ */
+export default function getFileName (path) {
+    path ??= '';
+
+    if (typeof path !== 'string') {
+        path = path.toString();
+    }
+
+    path = getAbsolutePath(path);
+
+    if (isDirectory(path)) {
+        return '';
+    }
+
+    return pathlib.basename(path);
+}
--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,5 @@
 export { default as getAbsolutePath } from "./functions/getAbsolutePath.mjs";
 export { default as getFileExtension } from "./functions/getFileExtension.mjs";
+export { default as getFileName } from "./functions/getFileName.mjs";
 export { default as isDirectory } from "./functions/isDirectory.mjs";
 export { default as setFileExtension } from "./functions/setFileExtension.mjs";
