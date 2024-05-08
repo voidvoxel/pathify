067d9cf3bec67f37440092e303e9e6ea7207d523
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-07 23:32:05 -0400

    Add tests

--- tests/functions/getAbsolutePath.test.js
+++ tests/functions/getAbsolutePath.test.js
@@ -2,6 +2,7 @@ import { default as pathlib } from "path";
 
 
 import { getAbsolutePath } from "../../src/index.mjs";
+import { homedir } from "os";
 
 
 test(
@@ -12,6 +13,64 @@ test(
         ).toBe(
             pathlib.resolve(
                 process.cwd()
+            )
+        );
+    }
+);
+
+
+test(
+    `getAbsolutePath('')`,
+    () => {
+        expect(
+            getAbsolutePath('')
+        ).toBe(
+            pathlib.resolve(
+                process.cwd()
+            )
+        );
+    }
+);
+
+
+test(
+    `getAbsolutePath(".")`,
+    () => {
+        expect(
+            getAbsolutePath(".")
+        ).toBe(
+            pathlib.resolve(
+                process.cwd()
+            )
+        );
+    }
+);
+
+
+test(
+    `getAbsolutePath("..")`,
+    () => {
+        expect(
+            getAbsolutePath("..")
+        ).toBe(
+            pathlib.resolve(
+                pathlib.join(
+                    ".."
+                )
+            )
+        );
+    }
+);
+
+
+test(
+    `getAbsolutePath("~")`,
+    () => {
+        expect(
+            getAbsolutePath("~")
+        ).toBe(
+            pathlib.resolve(
+                homedir()
             )
         );
     }
