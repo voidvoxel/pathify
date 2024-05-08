81860d16354e1cea6880c869f3ac7b66e4e536e5
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-07 23:25:08 -0400

    Create getAbsolutePath.test.js

---
+++ tests/functions/getAbsolutePath.test.js
@@ -0,0 +1,18 @@
+import { default as pathlib } from "path";
+
+
+import { getAbsolutePath } from "../../src/index.mjs";
+
+
+test(
+    "getAbsolutePath()",
+    () => {
+        expect(
+            getAbsolutePath()
+        ).toBe(
+            pathlib.resolve(
+                process.cwd()
+            )
+        );
+    }
+);
